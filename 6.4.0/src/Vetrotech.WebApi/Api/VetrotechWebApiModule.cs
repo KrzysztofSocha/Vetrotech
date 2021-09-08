a62d9f34d5099ef236fe01c0926c2887163a04a6
KrzysztofSocha <krzyssocha27@gmail.com>
2021-09-08 11:21:06 +0200

    Swagger-integration

--- 6.4.0/src/Vetrotech.WebApi/Api/VetrotechWebApiModule.cs
+++ 6.4.0/src/Vetrotech.WebApi/Api/VetrotechWebApiModule.cs
@@ -1,9 +1,11 @@
-﻿using System.Reflection;
+﻿using System.Linq;
+using System.Reflection;
 using System.Web.Http;
 using Abp.Application.Services;
 using Abp.Configuration.Startup;
 using Abp.Modules;
 using Abp.WebApi;
+using Swashbuckle.Application;
 
 namespace Vetrotech.Api
 {
@@ -19,6 +21,20 @@ namespace Vetrotech.Api
                 .Build();
 
             Configuration.Modules.AbpWebApi().HttpConfiguration.Filters.Add(new HostAuthenticationFilter("Bearer"));
+            ConfigureSwaggerUi();
+        }
+        private void ConfigureSwaggerUi()
+        {
+            Configuration.Modules.AbpWebApi().HttpConfiguration
+                .EnableSwagger(c =>
+                {
+                    c.SingleApiVersion("v1", "SwaggerIntegrationDemo.WebApi");
+                    c.ResolveConflictingActions(apiDescriptions => apiDescriptions.First());
+                })
+                .EnableSwaggerUi(c =>
+                {
+                    c.InjectJavaScript(Assembly.GetAssembly(typeof(VetrotechWebApiModule)), "Vetrotech.Api.Scripts.Swagger-Custom.js");
+                });
         }
     }
 }
